55c4bd960ca75b97d3867c0b5b0dae04e78c8fca
nicolaiharbo <nicolai@famharbo.dk>
2017-02-01 11:41:57 +0100

    Initial Commit

---
+++ testDemo1_triangles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testDemo1_triangles
+//
+//  Created by Nicolai Harbo on 01/02/2017.
+//  Copyright © 2017 nicowear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
